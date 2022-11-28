5e317c0036032ce12e2aa66f582770fe04689e2a
Domy <Domenico.marabello16@gmail.com>
2022-11-28 12:12:23 +0100

    Implemented cookies to be used with Chart.js - cleaned up some stuff

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,33 @@
 var express = require('express');
 var router = express.Router();
 const {
-  locationInfo,
-  getInfo
+    locationInfo,
+    getInfo
 } = require("../public/js/API_integration.js");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Homepage'
-  });
+router.get('/', function(req, res, next) {
+    res.render('index', {
+        title: 'Homepage'
+    });
 });
 
 
-router.post('/', async function (req, res, next) {
-  let city = req.body.location;
-  let info = await getInfo(city);
-  res.render('weather-card', {
-    info: info,
-  });
+router.post('/', async function(req, res, next) {
+    let city = req.body.location;
+    let info = await getInfo(city);
+    let hourly_temps = info.weather.hourly;
+
+    for (let i = 0; i < info.weather.hourly.length; i++) {
+        hourly_temps[i] = info.weather.hourly[i].temp;
+    }
+    res.cookie("hourly_temps", hourly_temps, { maxAge: 30000 });
+
+    res.render('weather-card', {
+        info: info,
+    });
+
+
 
 });
 
