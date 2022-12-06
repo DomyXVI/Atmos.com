a3d2b9d786aab21ab42c70e43c9e252f1d7eb42f
DomyXVI <49761478+DomyXVI@users.noreply.github.com>
2022-12-06 02:07:46 +0100

    modified port

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,7 +38,7 @@ app.use(function(err, req, res, next) {
 console.log("\u001b[1;33mConnection Established" + "\u001b[0m");
 console.log("\u001b[1;32mServer uptime: " + process.uptime() + " sec\u001b[0m");
 
-app.listen(80 || 3000);
+app.listen(5000);
 
 function intervalFunc() {
     console.log("\u001b[1;32mServer uptime: " + msToTime(process.uptime()) + " min\u001b[0m");
